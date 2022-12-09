fb2b6dbb95a33dba760b22d63d5f88cea02324df
Walachewaka <nomisbigcow@hotmail.ca>
2022-12-09 04:21:55 -0500

    Update Dashboard.vue

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -49,29 +49,34 @@
       </section>
     </div>
   </template>
-  <script>
-  import { mapGetters, mapActions } from 'vuex';
-  export default {
-    name: 'Dashboard',
-    data() {
-      return {
-        form: {
-          title: '',
-          content: '',
+<script>
+    import {
+        mapGetters,
+        mapActions
+    } from 'vuex';
+    export default {
+        name: 'Dashboard',
+        data() {
+            return {
+                form: {
+                    title: '',
+                    content: '',
+                },
+            };
         },
-      };
-    },
-    created: function() {
-      return this.$store.dispatch('getNotes');
-    },
-    computed: {
-      ...mapGetters({ notes: 'stateNotes'}),
-    },
-    methods: {
-      ...mapActions(['createNote']),
-      async submit() {
-        await this.createNote(this.form);
-      },
-    },
-  };
-  </script>
+        created: function() {
+            return this.$store.dispatch('getNotes');
+        },
+        computed: {
+            ...mapGetters({
+                notes: 'stateNotes'
+            }),
+        },
+        methods: {
+            ...mapActions(['createNote']),
+            async submit() {
+                await this.createNote(this.form);
+            },
+        },
+    };
+</script>

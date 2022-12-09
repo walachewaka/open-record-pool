5e2229c903b6306965676bdedd0713b1e043b00d
Walachewaka <nomisbigcow@hotmail.ca>
2022-12-09 03:46:55 -0500

    cleaning up & corrections

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,41 +3,25 @@
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="/">Open Record Pool</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul v-if="isLoggedIn" class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">Home</li>
+            <li class="nav-item">Dashboard</li>
+            <li class="nav-item">My Profile</li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/profile">My Profile</router-link>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" @click="logout">Log Out</a>
+              <a class="nav-link">Log Out</a>
             </li>
           </ul>
-          <ul v-else class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/register">Register</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/login">Log In</router-link>
-            </li>
+          <ul v-else="" class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">Home</li>
+            <li class="nav-item">Register</li>
+            <li class="nav-item">Log In</li>
           </ul>
         </div>
       </div>
     </nav>
   </header>
 </template>
-
 <script>
 export default {
   name: 'NavBar',
@@ -54,8 +38,9 @@ export default {
   },
 }
 </script>
+<style scoped="">
 
-<style scoped>
+
 a {
   cursor: pointer;
 }

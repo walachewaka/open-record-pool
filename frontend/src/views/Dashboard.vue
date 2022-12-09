4756bd8239f17e977431af990340cc3cb4408230
Walachewaka <nomisbigcow@hotmail.ca>
2022-12-09 03:49:01 -0500

    corrections cleaning up

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,53 +2,42 @@
   <div>
     <section>
       <h1>Add new note</h1>
-      <hr/><br/>
-
-      <form @submit.prevent="submit">
+      <hr>
+      <br>
+      <form>
         <div class="mb-3">
-          <label for="title" class="form-label">Title:</label>
-          <input type="text" name="title" v-model="form.title" class="form-control" />
+          <label for="title" class="form-label">Title:</label> <input type="text" name="title" v-model="form.title" class="form-control">
         </div>
         <div class="mb-3">
-          <label for="content" class="form-label">Content:</label>
-          <textarea
-            name="content"
-            v-model="form.content"
-            class="form-control"
-          ></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+          <label for="content" class="form-label">Content:</label> 
+          <textarea name="content" v-model="form.content" class="form-control"></textarea>
+        </div><button type="submit" class="btn btn-primary">Submit</button>
       </form>
-    </section>
-
-    <br/><br/>
-
+    </section><br>
+    <br>
     <section>
       <h1>Notes</h1>
-      <hr/><br/>
-
+      <hr>
+      <br>
       <div v-if="notes.length">
-        <div v-for="note in notes" :key="note.id" class="notes">
+        <div v-for="note in notes" class="notes">
           <div class="card" style="width: 18rem;">
             <div class="card-body">
               <ul>
                 <li><strong>Note Title:</strong> {{ note.title }}</li>
                 <li><strong>Author:</strong> {{ note.author.username }}</li>
-                <li><router-link :to="{name: 'Note', params:{id: note.id}}">View</router-link></li>
+                <li>View</li>
               </ul>
             </div>
-          </div>
-          <br/>
+          </div><br>
         </div>
       </div>
-
-      <div v-else>
+      <div v-else="">
         <p>Nothing to see. Check back later.</p>
       </div>
     </section>
   </div>
 </template>
-
 <script>
 import { mapGetters, mapActions } from 'vuex';
 export default {

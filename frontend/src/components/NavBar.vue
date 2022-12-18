9e9d5ab2461369b856f25a128327ce3966109a92
Walachewaka <nomisbigcow@hotmail.ca>
2022-12-18 17:57:46 -0500

    update navbar frontend warnings

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,20 +2,44 @@
   <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="/">Open Record Pool</a>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul v-if="isLoggedIn" class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">Home</li>
-            <li class="nav-item">Dashboard</li>
-            <li class="nav-item">My Profile</li>
+        <a
+          class="navbar-brand"
+          href="/"
+        >Open Record Pool</a>
+        <div
+          id="navbarCollapse"
+          class="collapse navbar-collapse"
+        >
+          <ul
+            v-if="isLoggedIn"
+            class="navbar-nav me-auto mb-2 mb-md-0"
+          >
+            <li class="nav-item">
+              Home
+            </li>
+            <li class="nav-item">
+              Dashboard
+            </li>
+            <li class="nav-item">
+              My Profile
+            </li>
             <li class="nav-item">
               <a class="nav-link">Log Out</a>
             </li>
           </ul>
-          <ul v-else="" class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">Home</li>
-            <li class="nav-item">Register</li>
-            <li class="nav-item">Log In</li>
+          <ul
+            v-else
+            class="navbar-nav me-auto mb-2 mb-md-0"
+          >
+            <li class="nav-item">
+              Home
+            </li>
+            <li class="nav-item">
+              Register
+            </li>
+            <li class="nav-item">
+              Log In
+            </li>
           </ul>
         </div>
       </div>
